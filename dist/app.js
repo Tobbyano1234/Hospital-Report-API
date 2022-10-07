486cd829c7ee1cdc2d4e41aa79ce0b66b28e55cd
tobbyano1234 <adegbesan86@gmail.com>
2022-10-08 00:53:51 +0100

    update

--- dist/app.js
+++ dist/app.js
@@ -9,7 +9,6 @@ const path_1 = __importDefault(require("path"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const database_config_1 = __importDefault(require("./config/database.config"));
-const cors_1 = __importDefault(require("cors"));
 database_config_1.default.sync()
     // db.sync({ force: true })
     .then(() => {
@@ -31,7 +30,7 @@ app.use((0, morgan_1.default)("dev"));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)());
+// app.use(cors());
 app.use(express_1.default.static(path_1.default.join(__dirname, "public")));
 // app.use("/", viewsRouter);
 // app.use("/", () => {});
