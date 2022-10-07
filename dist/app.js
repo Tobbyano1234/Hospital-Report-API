d759c9b1d9d35a5db7e0342dca4f197403a73083
tobbyano1234 <adegbesan86@gmail.com>
2022-10-08 00:16:41 +0100

    update

--- dist/app.js
+++ dist/app.js
@@ -31,7 +31,7 @@ app.use((0, morgan_1.default)("dev"));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)("*"));
 app.use(express_1.default.static(path_1.default.join(__dirname, "public")));
 // app.use("/", viewsRouter);
 // app.use("/", () => {});
