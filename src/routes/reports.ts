8c15b548d244f742e042df2ef01677f55844242a
tobbyano1234 <adegbesan86@gmail.com>
2022-10-08 03:32:16 +0100

    code clean up

--- src/routes/reports.ts
+++ src/routes/reports.ts
@@ -11,9 +11,9 @@ import { auth } from "../middleware/auth";
 const router = express.Router();
 
 router.post("/create", auth, PatientRecord);
-router.get("/read",auth, getPatientRecord);
-router.get("/read/:patientId", auth, getSinglePatientRecord);
-router.patch("/update/:patientId", auth, updatePatientRecord);
+router.get("/read", auth, getPatientRecord);
+router.get("/read", auth, getSinglePatientRecord);
+router.patch("/update", auth, updatePatientRecord);
 router.delete("/delete/:patientId", auth, deletePatientRecord);
 
 export default router;
